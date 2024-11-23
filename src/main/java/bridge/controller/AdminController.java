817a318e4f72406917c668e28253db9ec12cff53
dlaej <53420939+dlaej@users.noreply.github.com>
2024-11-23 20:20:15 +0900

    20241123_02 코드 설명

--- src/main/java/bridge/controller/AdminController.java
+++ src/main/java/bridge/controller/AdminController.java
@@ -50,9 +50,16 @@ public class AdminController {
 	메서드 종류로 get, post, put, delete 등이 있습니다.
 	그 중 get 요청은 클라이언트에서 서버로 정보를 요청하기 위해 사용되는 http 메소드입니다.
 	신고 목록 조회 기능은 단순히 서버로 데이터를 요청하는 기능이기 때문에 GetMapping을 사용했습니다. 
+	"/api/openReportList" get 요청이 오면 openReportList() 메서드가 실행됩니다.
 
 	3. ResponseEntity
-	@see bridge.controller.AdminController#selectReportCount()
+	Spring MVC에서 HTTP 요청에 대한 응답을 제어하는 데 주로 사용하는 클래스입니다.
+	Body, Header, Status를 포함할 수 있습니다.
+	해당 코드에서는 Body에 신고 목록 데이터와 상태 코드를 반환했습니다. 
+
+	4. bridgeService
+	비즈니스 로직을 구현하는 인터페이스입니다. 
+	부가 로직과 별도로 분리함으로써 
 
 	*/
 
