19822b6260edf0c9d90dc36e2c04b3fe0eb15cd0
dlaej <53420939+dlaej@users.noreply.github.com>
2024-11-23 20:01:02 +0900

    코드 설명을 위한 상세 주석 추가

--- src/main/java/bridge/controller/AdminController.java
+++ src/main/java/bridge/controller/AdminController.java
@@ -40,6 +40,21 @@ public class AdminController {
 			return ResponseEntity.status(HttpStatus.OK).body(list);
 		}
 	}
+	/* 신고 목록을 조회하는 기능입니다.
+	1. ApiOperation
+	Swagger란 Rest Api를 편리하게 문서화해주는 프레임워크입니다. 
+	Swagger가 제공하는 어노테이션 중 각각의 API에 대한 설명을 달아 놓기 위해 ApiOperation 어노테이션을 사용하였습니다. 
+ 
+	2. @GetMapping 
+	@RequestMapping은 http 메서드 요청을 처리할 수 있는 어노테이션입니다.
+	메서드 종류로 get, post, put, delete 등이 있습니다.
+	그 중 get 요청은 클라이언트에서 서버로 정보를 요청하기 위해 사용되는 http 메소드입니다.
+	신고 목록 조회 기능은 단순히 서버로 데이터를 요청하는 기능이기 때문에 GetMapping을 사용했습니다. 
+
+	3. ResponseEntity
+	@see bridge.controller.AdminController#selectReportCount()
+
+	*/
 
 	@ApiOperation(value = "특정 신고 목록 조회")
 	@GetMapping("/api/openReportDetail/{reportIdx}")
